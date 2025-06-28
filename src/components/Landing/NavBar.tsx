4e9c490769017e904d5c18b8f609ec2de0a25d70
Jason Fong <jasonf0286@gmail.com>
2025-06-28 11:44:41 +0800

    solve header nav bar

--- src/components/Landing/NavBar.tsx
+++ src/components/Landing/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     { id: "about", label: "About" },
     { id: "education", label: "Education" },
     { id: "skills", label: "Skills" },
-    { id: "projects", label: "Projects" },
+    // { id: "projects", label: "Projects" },
     { id: "contact", label: "Contact" },
   ];
 
